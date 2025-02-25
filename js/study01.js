9992a7327e5c5e7ecbf05f86a721ca3d7816b5f6
noeynuengthida <s6419410006@sau.ac.th>
2025-02-25 12:32:04 +0700

    studyjsfinal

--- js/study01.js
+++ js/study01.js
@@ -16,3 +16,21 @@ let data02 = 123456
 console.log('AAA ' + 555 + '  ' + data01 + '     ' + data02 )
 
 console.log(`AAA ${555}  ${data01}     ${data02}`)
+//study01.js
+//data type in javascript
+ 
+//String
+console.log("ABC..."); console.log('ABC')
+//Number
+console.log(123456) ;
+console.log(115.546)
+//Boolean
+console.log(true)  //false
+//Array
+console.log([1, 2, 3, 4, 5, 'Hello...', true])
+//Object
+console.log({fullname:'John', age:20})
+//Null
+console.log(null);
+//Undefined
+console.log(undefined)
